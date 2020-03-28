f6a41c077094a623268436110f29c7e2b5add6d1
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2020-03-28 15:56:46 -0400

    one away

---
+++ strings/OneAway.py
@@ -0,0 +1,35 @@
+def one_away(s1, s2):
+
+    m = len(s1)
+    n = len(s2)
+
+    if abs(m-n) > 1:
+        return False
+    i = 0
+    j = 0
+    count = 0
+    while i < m and j < n:
+
+        if s1[i] != s2[j]:
+            if count == 1:
+                return False
+
+            if m > n:
+                j += 1
+            elif n > m:
+                i += 1
+            else:
+                i += 1
+                j += 1
+            count += 1
+        else:
+            i += 1
+            j += 1
+
+    if i < m or j < n:
+        count += 1
+
+    return count == 1
+
+
+print(one_away("abc", "abcd"))
